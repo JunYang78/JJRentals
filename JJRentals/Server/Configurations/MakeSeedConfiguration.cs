a06004ef273977d4b44398b7daafb870919dc212
ER JUN YANG 2201261A <2201261A@student.tp.edu.sg>
2023-12-03 20:02:34 +0800

    Add project files.

---
+++ JJRentals/Server/Configurations/MakeSeedConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using JJRentals.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JJRentals.Server.Configurations
+{
+    public class MakeSeedConfiguration : IEntityTypeConfiguration<Make>
+    {
+        public void Configure(EntityTypeBuilder<Make> builder)
+        {
+            builder.HasData(
+                new Make
+                {
+                    Id = 1,
+                    Name = "BMW",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Make
+                {
+                    Id = 2,
+                    Name = "Toyota",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+            );
+        }
+    }
+}
