23fee172723c78f6c99be176b72298fb38449a90
ER JUN YANG 2201261A <2201261A@student.tp.edu.sg>
2024-01-16 15:15:45 +0800

    Add project files.

---
+++ JJRentals/Server/Controllers/MakesController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JJRentals.Server.IRepository;
+using JJRentals.Shared.Domain;
+
+namespace JJRentals.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MakesController : ControllerBase
+    {
+        //Refactored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //Refactored
+        //public MakesController(ApplicationDbContext context)
+        public MakesController(IUnitOfWork unitOfWork)
+        {
+            //Refactored
+            //_context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Makes
+        [HttpGet]
+        //public async Task<ActionResult<IEnumerable<Make>>> GetMakes()
+        public async Task<IActionResult> GetMakes()
+        {
+            //Refactored
+            if (_unitOfWork.Makes == null)
+            {
+                return NotFound();
+            }
+
+            //return await _context.Makes.ToListAsync();
+            var makes = await _unitOfWork.Makes.GetAll();
+            return Ok(makes);
+        }
+
+        // GET: api/Makes/5
+        [HttpGet("{id}")]
+        //public async Task<ActionResult<Make>> GetMake(int id)
+        public async Task<IActionResult> GetMakes(int id)
+        {
+            if (_unitOfWork.Makes == null)
+            {
+                return NotFound();
+            }
+            //var make = await _context.Makes.FindAsync(id);
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id); /* lamda expression: find instance of q, where q is of "Makes" type, so finding a instance of q with a specific Id that is equals to the id parsed*/
+
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(make);
+        }
+
+        // PUT: api/Makes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMake(int id, Make make)
+        {
+            if (id != make.Id)
+            {
+                return BadRequest();
+            }
+
+            //Refactored
+            //_context.Entry(make).State = EntityState.Modified;
+            _unitOfWork.Makes.Update(make);
+
+            try
+            {
+                //Refactored
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Refactored
+                //if (!MakeExists(id))
+                if (!await MakeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Makes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostMake(Make make)
+        {
+            if (_unitOfWork.Makes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Makes'  is null.");
+            }
+            await _unitOfWork.Makes.Insert(make);
+            await _unitOfWork.Save(HttpContext);
+            //_context.Makes.Add(make);
+            //await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMake", new { id = make.Id }, make);
+        }
+
+        // DELETE: api/Makes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            //if (_context.Makes == null)
+            //{
+            // return NotFound();
+            // }
+
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+            //var make = await _context.Makes.FindAsync(id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            ///_context.Makes.Remove(make);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Makes.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> MakeExists(int id)
+        {
+            //return (_context.Makes?.Any(e => e.Id == id)).GetValueOrDefault();
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+            return make != null;
+        }
+    }
+}
---
+++ JJRentals/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using JJRentals.Shared.Domain;
+
+namespace JJRentals.Server.IRepository
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		Task Save(HttpContext httpContext);
+		IGenericRepository<Make> Makes { get; }
+		IGenericRepository<Model> Models { get; }
+		IGenericRepository<Vehicle> Vehicles { get; }
+		IGenericRepository<Colour> Colours { get; }
+		IGenericRepository<Booking> Bookings { get; }
+		IGenericRepository<Customer> Customers { get; }
+	}
+}
