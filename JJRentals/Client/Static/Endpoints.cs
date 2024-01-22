327fab350a89a8a0996614f1c08e6ccd7c9a7f19
HOR WEN XI JOSEPH 2204128I <2204128I@student.tp.edu.sg>
2024-01-22 22:37:25 +0800

    added crud func

--- JJRentals/Client/Static/Endpoints.cs
+++ JJRentals/Client/Static/Endpoints.cs
@@ -8,10 +8,14 @@
         public static readonly string MakesEndpoint = $"{Prefix}/manus";
         public static readonly string ModelsEndpoint = $"{Prefix}/models";
         public static readonly string ColoursEndpoint = $"{Prefix}/colours";
-        public static readonly string carsEndpoint = $"{Prefix}/cars";
+        public static readonly string CarsEndpoint = $"{Prefix}/cars";
         public static readonly string CustomersEndpoint = $"{Prefix}/customers";
         public static readonly string BookingsEndpoint = $"{Prefix}/bookings";
         public static readonly string PaymentsEndpoint = $"{Prefix}/payments";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+        public static readonly string OutletsEndpoint = $"{Prefix}/outlets";
+        public static readonly string MaintenancesEndpoint = $"{Prefix}/maintenances";
+        public static readonly string ImagesEndpoint = $"{Prefix}/images";
     }
 
 
